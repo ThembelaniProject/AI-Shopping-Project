aee18a6587cf20481305a38e3a8508d6bbf90c04
Thembelani <thembelanibuthelezi64@gmail.com>
2026-09-19 06:49:46 +0200

    removing microsoft login since there is no Client and tenant id

--- accounts/urls.py
+++ accounts/urls.py
@@ -43,17 +43,7 @@ path(
 # MICROSOFT LOGIN
 # ------------------------------------------------------
 
-path(
-    "microsoft/login/",
-    views.microsoft_login,
-    name="microsoft_login",
-),
 
-path(
-    "microsoft/callback/",
-    views.microsoft_callback,
-    name="microsoft_callback",
-),
 
 # ------------------------------------------------------
 # PROFILE
